2c5a777a76c3ec306a70cff3c677c71a8e08ee26
david mulligan <s00155368@mail.itsligo.ie>
2019-03-29 14:11:34 +0000

    refactor from project sample template

---
+++ Rad302Summer2019ConsoleClientdmulligan/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rad302Summer2019ConsoleClientdmulligan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
