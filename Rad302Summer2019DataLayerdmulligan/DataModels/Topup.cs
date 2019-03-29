28a096fc5809530def690f425e053b0a3ceb8f09
david mulligan <s00155368@mail.itsligo.ie>
2019-03-29 14:37:18 +0000

    created topup data model and topup history

---
+++ Rad302Summer2019DataLayerdmulligan/DataModels/Topup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rad302Summer2019DataLayerdmulligan.DataModels
+{
+    class Topup
+    {
+        public int      TransactionID { get; set; }
+        public int      CustomerID    { get; set; }
+        public DateTime Date          { get; set; }
+        public decimal  Amount        { get; set; }
+
+        // a copy of all transactions will be stored here
+        public static List<Topup> History = new List<Topup>();
+
+        public static void MakeTransaction(Topup topup) => History.Add(topup);
+
+    }
+}
