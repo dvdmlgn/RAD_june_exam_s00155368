974aba978145ad1906eaed0a24e85dab20ca5dd0
david mulligan <s00155368@mail.itsligo.ie>
2019-03-29 14:30:25 +0000

    created customer data model

---
+++ Rad302Summer2019DataLayerdmulligan/DataModels/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rad302Summer2019DataLayerdmulligan.DataModels
+{
+    public class Customer
+    {
+        public int      CustomerID           { get; set; }
+        public string   CustomerName         { get; set; }
+        public DateTime RegisteredOn         { get; set; }
+        public decimal  CurrentCreditBalance { get; set; }
+
+
+        public void TopupAccount(decimal amount)
+        {
+
+        }
+
+        public void Purchase()
+        {
+
+        }
+    }
+}
