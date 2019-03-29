064bd98a6af2703cfd8b5f25f8840ae081e37a00
david mulligan <s00155368@mail.itsligo.ie>
2019-03-29 14:53:28 +0000

    created purchase data model and history store data structure

---
+++ Rad302Summer2019DataLayerdmulligan/DataModels/Purchase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rad302Summer2019DataLayerdmulligan.DataModels
+{
+    class Purchase
+    {
+        public int      TransactionID     { get; set; }
+        public int      CustomerID        { get; set; }
+        public int      ResourceID        { get; set; }
+        public DateTime DateOfPurchase    { get; set; }
+        public int      QuantityPurchased { get; set; }
+
+
+        public Purchase(int customerId, int resourceId, int quantity)
+        {
+            TransactionID     = Guid.NewGuid().GetHashCode();
+            CustomerID        = customerId;
+            ResourceID        = resourceId;
+            DateOfPurchase    = DateTime.UtcNow;
+            QuantityPurchased = quantity;
+        }
+
+        // a copy of all purchases will be stored here
+        public static List<Purchase> History = new List<Purchase>();
+
+        public static void MakePurchase(Purchase purchase) => History.Add(purchase);
+    }
+}
